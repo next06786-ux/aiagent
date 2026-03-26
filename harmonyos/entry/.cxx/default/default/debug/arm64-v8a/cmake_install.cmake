e7ee324735e8e579d929a121860be913e489dd43
Next06786-ux <next06786@gmail.com>
2026-03-26 10:33:23 +0800

    feat: enhance decision simulation streaming and canvas UX

--- harmonyos/entry/.cxx/default/default/debug/arm64-v8a/cmake_install.cmake
+++ harmonyos/entry/.cxx/default/default/debug/arm64-v8a/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/starmap_render")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/native_renderers")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
